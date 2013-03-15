282166e7e86562c6dcb7f5f8297844111898744d
janetchang <janetlaichang@gmail.com>
2013-03-14 19:27:33 -0700

    clean up old comments, attempt remote_repo_path

--- codeclimatestatus.py
+++ codeclimatestatus.py
@@ -1,12 +1,6 @@
-import sublime, sublime_plugin, threading, json, os
-
-
-def find_repo_path(full_name):
-  print os.path.split(full_name)
-  
+import sublime, sublime_plugin, subprocess, json, os
 
 class CodeClimateStatusListener(sublime_plugin.EventListener):
-  #start getting CodeClimate ratings when file loads
   def on_load(self, view):
     if 'source.ruby' not in view.scope_name(0):
       return
@@ -14,9 +8,6 @@ class CodeClimateStatusListener(sublime_plugin.EventListener):
 
 class CodeClimateStatusCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-  # determine if current file is a class
-    if not is_valid_klass?
-      return
   #--------------------------------------make threads for a class
     #thread = CodeClimateAPICall() 
     #threads.apprend(thread)
@@ -25,29 +16,19 @@ class CodeClimateStatusCommand(sublime_plugin.TextCommand):
     #self.view.klass().clear() #klass or klasses?
   #make codeclimate key object
   #--------------------------------------threads
-  #handle all threads
-  #next_threads
-  #for thread in threads:
-    #if thread.is_alive():
-      #next_threads.append(thread)
-      #continue
-    #if thread.result == False:
-      #continue
-  #threads = next_threads
+  #if thread.is_alive():
+    #continue
+  #if thread.result == False:
+    #thread = nil
   #--------------------------------------
     #plugin_settings = sublime.load_settings('CodeClimateStatus.sublime-theme')
-
 
     grade = "D"
     old_gutter_grade = self.view.get_regions('gutter-grade')
 
     self.view.sel().clear()
-
-    self.view.erase_regions('gutter-grade')
+    self.view.erase_regions('old_gutter-grade')
     self.view.add_regions('gutter-grade', [sublime.Region(self.view.text_point(0, 0))], 'constant', self.bookmark_path(grade))
-    # self.view.add_regions('gutter-grade-b', [sublime.Region(self.view.text_point(1, 0))], 'constant', '../CodeClimateStatus/img/letterB')
-    # self.view.add_regions('gutter-grade-c', [sublime.Region(self.view.text_point(2, 0))], 'constant', '../CodeClimateStatus/img/letterC')
-    # self.view.add_regions('gutter-grade-d', [sublime.Region(self.view.text_point(3, 0))], 'constant', '../CodeClimateStatus/img/letterD')
 
     klass = "ExampleClass"
     complexity = "LOW"
@@ -58,11 +39,26 @@ class CodeClimateStatusCommand(sublime_plugin.TextCommand):
     self.view.set_status('code-climate', status)
 
     print old_gutter_grade
-    print find_repo_path()
+    
+    #get full file path
+    full_path = self.view.file_name()
+    local_dir = local_repo_path(full_path)
+    print remote_repo_path(local_dir)
 
   def bookmark_path(self, grade):
     return '../CodeClimateStatus/img/letter' + grade
 
+def local_repo_path(original_path):
+  return "bookshelf"
+  #original_path eg. "/Users/janet/bookshelf/app/models/book.rb"
+  #print os.path.split(full_name)
+  #result eg. "bookshelf"
+
+def remote_repo_path(local_repo):
+  os.chdir('../bookshelf/')
+  git_command = ["git", "config", "--get", "remote.origin.url"]
+  popen = subprocess.Popen(git_command, stdout = subprocess.PIPE)
+  return popen.communicate()[0].strip()
 
 # class CodeClimateAPICall(threading.Thread):
 #   def __init__(self, klass, timeout):
